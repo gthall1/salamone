00d6e43bc28c98c5361ea4ee171752027fe48425
Griff Hall <griffhall1@gmail.com>
2017-01-22 12:57:51 -0600

    changed to index for pusing live

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Email: $email \n Phone: $phone \n Message: $message";
-$recipient = "griff@getluckee.com";
+$recipient = "griff@rockbottomcreativeagency.com";
 $subject = "Website Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
